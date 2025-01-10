98fd32c079cce620c6a28cedddabd455a11fcb5f
Ed Wright <ewright@digitalfutures.com>
2025-01-10 19:00:43 +0000

    Add type hinting to extract_data and return tuple of dfs

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -3,9 +3,9 @@ from etl.extract.extract_transactions import extract_transactions
 from etl.extract.extract_customers import extract_customers
 
 
-def extract_data() -> pd.DataFrame:
+def extract_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     transactions = extract_transactions()
     customers = extract_customers()
     print(transactions)
     print(customers)
-    return None
+    return (transactions, customers)
