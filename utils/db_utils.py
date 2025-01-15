1f8ae7eadf983815228be454f419e1933ef3412a
Ed Wright <ewright@digitalfutures.com>
2025-01-15 11:32:42 +0000

    FIX STEP 1: change get_db_connection function to use SQLAlchemy

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,4 +1,5 @@
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 import logging
 from utils.logging_utils import setup_logger
 
@@ -15,13 +16,33 @@ class QueryExecutionError(Exception):
 logger = setup_logger(__name__, 'database.log', level=logging.DEBUG)
 
 
+# Issues being caused by using psycopg2 - better refactor to use SQLAlchemy
+# def get_db_connection(connection_params):
+#     try:
+#         connection = psycopg2.connect(**connection_params)
+#         logger.setLevel(logging.INFO)
+#         logger.info("Successfully connected to the database.")
+#         return connection
+#     except psycopg2.Error as e:
+#         logger.setLevel(logging.ERROR)
+#         logger.error(f"Failed to connect to the database: {e}")
+#         raise DatabaseConnectionError(
+#             f"Failed to connect to the database: {e}"
+#         )
+
+
 def get_db_connection(connection_params):
     try:
-        connection = psycopg2.connect(**connection_params)
+        # Create SQLAlchemy engine
+        engine = create_engine(
+            f"postgresql+psycopg2://{connection_params['user']}"
+            f":{connection_params['password']}@{connection_params['host']}"
+            f":{connection_params['port']}/{connection_params['dbname']}"
+        )
         logger.setLevel(logging.INFO)
         logger.info("Successfully connected to the database.")
-        return connection
-    except psycopg2.Error as e:
+        return engine
+    except SQLAlchemyError as e:
         logger.setLevel(logging.ERROR)
         logger.error(f"Failed to connect to the database: {e}")
         raise DatabaseConnectionError(
